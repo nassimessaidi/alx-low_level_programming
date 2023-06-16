607141b2a30c34548ced69bdc53876711089bbf6
Nassim ESSAIDI <contact@essaidi.me>
2023-06-16 14:49:54 -0700

    coding style issue, trailing whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,8 @@
  * Description: '0-positive_or_negative'
  *
  * Return: Always 0 (Success)
- * betty style doc for function main goes there 
- * */
+ * betty style doc for function main goes there
+*/
 int main(void)
 {
 	int n;
@@ -18,11 +18,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) 
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0) 
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	else 
+	else
 		printf("%d is zero\n", n);
 	return (0);
 }
