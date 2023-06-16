37ee6d4de0a44a5a89ef3c48fe14462be9021b31
Nassim ESSAIDI <contact@essaidi.me>
2023-06-16 08:23:05 -0700

    fix coding style again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,14 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: '0-positive_or_negative'
+ *
+ * Return: Always 0 (Success)
+ * betty style doc for function main goes there 
+ * */
 int main(void)
 {
 	int n;
@@ -11,9 +18,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0) 
+	if (n > 0) 
 		printf("%d is positive\n", n);
-	else if (n<0) 
+	else if (n < 0) 
 		printf("%d is negative\n", n);
 	else 
 		printf("%d is zero\n", n);
