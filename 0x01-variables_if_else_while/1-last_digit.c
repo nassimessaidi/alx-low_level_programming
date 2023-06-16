95955a66bef1938c529c9750fb4c1d9a4b8503b9
Nassim ESSAIDI <contact@essaidi.me>
2023-06-16 15:07:41 -0700

    fix coding style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: '1-last_digit.c'
+ *
+ * Return: Always 0 (Success)
+ * betty style doc for function main goes there
+*/
 int main(void)
 {
 	int n, last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
