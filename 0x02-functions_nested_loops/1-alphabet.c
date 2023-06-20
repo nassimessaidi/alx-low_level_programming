dd079e08b7b7045a209f9db7fc92edbfb10dd682
Nassim ESSAIDI <contact@essaidi.me>
2023-06-20 12:25:43 -0700

    add Betty documentation style deescription

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: '0-putchar'
- *
- * Return: Always 0 (Success)
+ * print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always void (Success)
  * betty style doc for function main goes there
 */
 
