75cd13218ab92bdc6b8ae599537093a20cbb5a2e
Nassim ESSAIDI <contact@essaidi.me>
2023-06-20 12:11:19 -0700

    add coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- * Description: prints _putchar, followed by a new line
+ * main - Entry point
+ *
+ * Description: '0-putchar'
+ *
  * Return: Always 0 (Success)
- */
+ * betty style doc for function main goes there
+*/
 
 int main(void)
 {
