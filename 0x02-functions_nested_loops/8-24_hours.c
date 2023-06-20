51228da838eeacce107d7a87a8dd22bc294b85dc
Nassim ESSAIDI <contact@essaidi.me>
2023-06-20 15:23:17 -0700

    fix coding and documentation style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer prints every minute of the day of Jack Bauer,
+ * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59
  * Return: absolute value of an integer
  * betty style doc for function main goes there
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	int i, j;
 
 	for (i = 0; i < 24; i++)
-		for (j = 0; j< 60; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
