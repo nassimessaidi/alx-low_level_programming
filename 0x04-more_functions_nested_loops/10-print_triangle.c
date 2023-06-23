7595f0f2c5462502843cb49ac6fe3a184375c93d
Nassim ESSAIDI <contact@essaidi.me>
2023-06-22 17:12:01 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,26 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-	
 	if (size <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
+	{
+		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
+			{
 				_putchar(' ');
+			}
 
 			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
+			}
 
 			_putchar('\n');
 		}
+	}
 }
