2806ca9770fc3962912cdf52446298729398c13a
Nassim ESSAIDI <contact@essaidi.me>
2023-06-26 14:21:22 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int long_str;
+
+	while (*s != '\0)
+	{
+		long_str++;
+		s++;
+	}
+
+	return (long_str);
+}
